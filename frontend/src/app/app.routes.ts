606603a6d0502cf73009fa8c29900050b4f94801
Yashaswini-Ashwath <yashaswini,gajendra@gmail.com>
2025-12-02 04:54:59 +0100

    removed unwanted comment

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { ListConfigsComponent } from './components/list-configs/list-configs';
 import { AddConfig } from './components/add-config/add-config';
 import { DetailConfig } from './components/detail-config/detail-config';
 
-// Define routes for list, add, and detail views
 export const routes: Routes = [
   { path: '', redirectTo: '/list', pathMatch: 'full' },
   { path: 'list', component: ListConfigsComponent },
